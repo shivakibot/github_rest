b075c3377e8a27676fa76600f0a7332446312d7e
shivakibot <thirst4for@gmail.com>
2023-02-22 17:07:36 +0900

    コメント追加

--- test/unit/home_vm_test.dart
+++ test/unit/home_vm_test.dart
@@ -7,12 +7,10 @@ import 'package:github_rest/viewModel/common.dart';
 import 'package:github_rest/viewModel/home_vm.dart';
 import 'github_rest_test.mocks.dart';
 
-void main(){
-  setUp(() {
-    // [MUST]
-  });
+void main() {
   final mockSearchProvider = StateProvider<String>((ref) => 'github_rest');
   group(const HomeState().runtimeType, () {
+    // 初期値が AsyncValue<List<SchemeRepository>>.loading()であるか
     test('init Loading', () {
       final container = ProviderContainer(
         overrides: [
@@ -23,6 +21,7 @@ void main(){
       const matcher = AsyncValue<List<SchemeRepository>>.loading();
       expect(actual, matcher);
     });
+    // 実際にREST APIをコールして AsyncValueとして出力されているか
     test('fetch myRepo', () async {
       final container = ProviderContainer(
         overrides: [
@@ -33,26 +32,29 @@ void main(){
       final vm = container.read(homeStateNotifier.notifier);
       vm.searchRepository();
       container.read(homeStateNotifier).when(
-          data: (data) {
-            expect(data.length, 1);
-            final repo = data[0];
-            expect(repo.fullName, 'shivakibot/github_rest');
-            expect(repo.owner.avatarUrl, 'https://avatars.githubusercontent.com/u/54189261?v=4');
-            expect(repo.language, 'Dart');
-            expect(repo.stargazersCount, 0);
-            expect(repo.watchersCount, 0);
-            expect(repo.forksCount, 0);
-            expect(repo.issuesCount, 0);
-          },
-          error: (error, stackTrace) {
-            fail('when error');
-          },
-          loading: () {
-            // 処理を継続
-          },
+        data: (data) {
+          expect(data.length, 1);
+          final repo = data[0];
+          expect(repo.fullName, 'shivakibot/github_rest');
+          expect(repo.owner.avatarUrl,
+              'https://avatars.githubusercontent.com/u/54189261?v=4');
+          expect(repo.language, 'Dart');
+          expect(repo.stargazersCount, 0);
+          expect(repo.watchersCount, 0);
+          expect(repo.forksCount, 0);
+          expect(repo.issuesCount, 0);
+        },
+        error: (error, stackTrace) {
+          fail('when error');
+        },
+        loading: () {
+          // 処理を継続
+        },
       );
     });
-    test('fetch first page(15 repository)', () async{
+    // searchRepository()を実行し、15件のgithub repository情報を出力できているか
+    // フォーマットチェックは github_rest_testで行っている為、対象外
+    test('fetch first page(15 repository)', () async {
       final container = ProviderContainer(
         overrides: [
           searchProvider.overrideWith((ref) => ref.read(mockSearchProvider)),
@@ -72,8 +74,10 @@ void main(){
           // 処理を継続
         },
       );
-
     });
+    // searchRepository()実行後。addPage()を実行し、
+    // 合計30件のgithub repository情報を出力できているか
+    // フォーマットチェックは github_rest_testで行っている為、対象外
     test('fetch add next page(30 repository)', () async {
       final container = ProviderContainer(
         overrides: [
